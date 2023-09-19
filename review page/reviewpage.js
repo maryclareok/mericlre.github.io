ba537e17eec18222fb8f15df87cb88c08a99ada8
maryclareok <66754810+maryclareok@users.noreply.github.com>
2023-09-19 09:54:00 +0100

    review webpage

---
+++ review page/reviewpage.js
@@ -0,0 +1,78 @@
+// local reviews data
+const reviews = [
+    {
+      id: 1,
+      name: 'susan smith',
+      job: 'web developer',
+      img: 'https://www.course-api.com/images/people/person-1.jpeg',
+      text: "I'm baby meggings twee health goth +1. Bicycle rights tumeric chartreuse before they sold out chambray pop-up. Shaman humblebrag pickled coloring book salvia hoodie, cold-pressed four dollar toast everyday carry",
+    },
+    {
+      id: 2,
+      name: 'anna johnson',
+      job: 'web designer',
+      img: 'https://www.course-api.com/images/people/person-2.jpeg',
+      text: 'Helvetica artisan kinfolk thundercats lumbersexual blue bottle. Disrupt glossier gastropub deep v vice franzen hell of brooklyn twee enamel pin fashion axe.photo booth jean shorts artisan narwhal.',
+    },
+    {
+      id: 3,
+      name: 'peter jones',
+      job: 'intern',
+      img: 'https://www.course-api.com/images/people/person-4.jpeg',
+      text: 'Sriracha literally flexitarian irony, vape marfa unicorn. Glossier tattooed 8-bit, fixie waistcoat offal activated charcoal slow-carb marfa hell of pabst raclette post-ironic jianbing swag.',
+    },
+    {
+      id: 4,
+      name: 'bill anderson',
+      job: 'the boss',
+      img: 'https://www.course-api.com/images/people/person-3.jpeg',
+      text: 'Edison bulb put a bird on it humblebrag, marfa pok pok heirloom fashion axe cray stumptown venmo actually seitan. VHS farm-to-table schlitz, edison bulb pop-up 3 wolf moon tote bag street art shabby chic. ',
+    },
+  ];
+  //select items
+  const img  = document.getElementById("person-img");
+  const author= document.getElementById("author");
+  const job= document.getElementById("job");
+  const info= document.getElementById("info");
+  const prevBtn= document.querySelector(".prev-btn");
+  const nextBtn= document.querySelector(".next-btn");
+  const randomBtn = document.querySelector(".random-btn");
+  // set starting item
+  let currentitem = 0;
+  //load inital item
+  window.addEventListener('DOMContentLoaded',function () {
+    showPerson(currentitem)
+
+  })
+     //random number generator
+     function rndfunc(){
+      return Math.floor(Math.random()* (reviews.length));
+     }
+  //show  person based on item
+  function showPerson(person){
+    console.log("shake and bake") ;
+    const item = reviews [person];
+    img.src = item.img   ;
+    author.textContent = item.name;
+    job.textContent = item.job;
+    info.textContent = item.text;
+  }
+  nextBtn.addEventListener('click',function(){
+    currentitem++ ;
+    if(currentitem>reviews.length-1){
+      currentitem = 0
+    }
+    showPerson(currentitem);
+  })
+  prevBtn.addEventListener('click',function(){
+    currentitem -- ;
+    if(currentitem < 0){
+      currentitem = reviews.length-1
+    }
+    showPerson(currentitem);
+  })
+//show random person
+  randomBtn.addEventListener('click',function(){
+    console.log (rndfunc())
+    showPerson(rndfunc());
+  })
